85d0aa942d601e4b0fe6e6cc957aeb3570311519
Peter-Frank Spierenburg <peter@ubuntu.linuxcertified.com>
2010-06-02 17:57:05 -0600

    ClickCommandProcessor now uses patent-pending first-come-first-server technology.

--- src/main/java/org/fitfest/core/ClickCommandProcessor.java
+++ src/main/java/org/fitfest/core/ClickCommandProcessor.java
@@ -1,5 +1,8 @@
 package org.fitfest.core;
 
+import javax.swing.JButton;
+
+import org.fest.swing.core.GenericTypeMatcher;
 import org.fest.swing.fixture.FrameFixture;
 
 public class ClickCommandProcessor implements CommandProcessor
@@ -11,7 +14,20 @@ public class ClickCommandProcessor implements CommandProcessor
 
     public void handleRow( final FrameFixture window, final RowHandler rowHandler )
     {
-        window.button( rowHandler.getText( 1 ) ).click();
+        window.button( new GenericTypeMatcher<JButton>(JButton.class) {
+            private boolean firstCome = true;
+            
+            @Override
+            protected boolean isMatching( JButton component )
+            {
+                if(firstCome && rowHandler.getText( 1 ).equals( component.getName() ) && component.isShowing()) {
+                    firstCome = false;
+                    return true;
+                }
+                return false;
+            }
+            
+        }).click();
         rowHandler.right( 1 );
     }
 }
